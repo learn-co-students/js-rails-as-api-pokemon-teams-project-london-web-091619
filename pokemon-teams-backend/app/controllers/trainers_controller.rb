29ba2451e96749265b2abd2ffbe9821773a2eddf
Tim Golding <tgolding55@hotmail.com>
2019-10-31 13:57:31 +0000

    Done.

---
+++ pokemon-teams-backend/app/controllers/trainers_controller.rb
@@ -0,0 +1,7 @@
+class TrainersController < ApplicationController
+    def index
+        trainers = Trainer.all
+        render json: trainers, only: [:id, :name], include: [pokemons: {only: [:id, :nickname, :species, :trainer_id]}]
+    end
+
+end
