ca9fbfe1bef5ebca37443dca4ca254cc7e15caa6
idmyn <hello@davidmyno.rs>
2019-11-01 10:04:50 +0000

    show all trainers and all pokemon on landing page

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,3 +1,43 @@
 const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
+
+fetchTrainers()
+
+function fetchTrainers() {
+  fetch(TRAINERS_URL)
+    .then(response => response.json())
+    .then(trainers => trainers.forEach(trainer => injectTrainer(trainer)))
+}
+
+function injectTrainer(trainer) {
+  const div = document.createElement("div")
+  div.classList.add("card")
+
+  const p = document.createElement("p")
+  p.textContent = trainer.name
+
+  const button = document.createElement("button")
+  button.setAttribute("data-trainer-id", trainer.id)
+  button.textContent = "Add Pokemon"
+
+  const ul = document.createElement("ul")
+  trainer.pokemons.forEach(pokemon => injectPokemon(pokemon, ul))
+
+  div.append(p, button, ul)
+  document.querySelector("main").append(div)
+}
+
+function injectPokemon(pokemon, ul) {
+  console.log(pokemon)
+  const li = document.createElement("li")
+  li.textContent = `${pokemon.nickname} (${pokemon.species})`
+
+  const button = document.createElement("button")
+  button.textContent = "Release"
+  button.classList.add("release")
+  button.setAttribute("data-pokemon-id", pokemon.id)
+
+  li.append(button)
+  ul.append(li)
+}
